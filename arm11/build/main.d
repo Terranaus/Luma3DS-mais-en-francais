f6f5d79e6604169cc445b077fd66c4d17eb9644b
Terranaus <124790184+Terranaus@users.noreply.github.com>
2025-04-16 21:28:55 +0200

    update des caractères

---
+++ arm11/build/main.d
@@ -0,0 +1,5 @@
+main.o: C:/Users/Terra/Luma3DS-mais-en-francais/arm11/source/main.c \
+ C:/Users/Terra/Luma3DS-mais-en-francais/arm11/source/types.h \
+ C:/Users/Terra/Luma3DS-mais-en-francais/arm11/source/memory.h
+C:/Users/Terra/Luma3DS-mais-en-francais/arm11/source/types.h:
+C:/Users/Terra/Luma3DS-mais-en-francais/arm11/source/memory.h:
