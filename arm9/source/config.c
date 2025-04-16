d00c7f2f05046a384fd8feb17449bec17bf449f6
Terranaus <124790184+Terranaus@users.noreply.github.com>
2025-04-16 22:13:04 +0200

    encore le problème d'avant

--- arm9/source/config.c
+++ arm9/source/config.c
@@ -1014,7 +1014,7 @@ void configMenu(bool oldPinStatus, u32 oldPinMode)
                                        "FIRM1" };
 
     drawString(true, 10, 10, COLOR_TITLE, CONFIG_TITLE);
-    drawString(true, 10, 10 + SPACING_Y, COLOR_TITLE, "Utiliser le DPAD et A pour changer les param\u00Etres");
+    drawString(true, 10, 10 + SPACING_Y, COLOR_TITLE, "Utiliser le DPAD et A pour changer les param\u00E8tres");
     drawFormattedString(false, 10, SCREEN_HEIGHT - 2 * SPACING_Y, COLOR_YELLOW, "D\u00E9marr\u00E9 depuis %s via %s", isSdMode ? "SD" : "CTRNAND", bootTypes[(u32)bootType]);
 
     //Character to display a selected option
